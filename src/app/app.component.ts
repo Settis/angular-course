063700db52c69c2877257edae4aacdf42dbd0cd3
Anton <strannik.anton@gmail.com>
2017-02-06 01:39:56 +0400

    Remove ngFor. Implement Place constructor. Move styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,54 +8,17 @@ import { Place } from './place'
 })
 export class AppComponent implements OnInit {
 
-  public places: Array<Place> = [{
-    name: 'Hotel',
-    mainImage: 'assets/images/1main.jpg',
-    mainImageMin: 'assets/images/1mainMin.jpg',
-    secondImage: 'assets/images/1second.jpg',
-    secondImageMin: 'assets/images/1secondMin.jpg',
-    country: 'Puerto Rico',
-    city: 'San Juan',
-    telephone: 123321123,
-    temperature: 33,
-    waterTemperature: 21,
-    weather: 'sunny',
-    followers: 45,
-    following: 3445
-  }, {
-    name: 'SPA',
-    mainImage: 'assets/images/2main.jpg',
-    mainImageMin: 'assets/images/2mainMin.jpg',
-    secondImage: 'assets/images/2second.jpg',
-    secondImageMin: 'assets/images/2secondMin.jpg',
-    country: 'French Polyneisa',
-    city: 'Bora Bora',
-    telephone: 123555123,
-    temperature: 30,
-    waterTemperature: 25,
-    weather: 'windy',
-    followers: 12345,
-    following: 405
-  }, {
-    name: 'Another hotel',
-    mainImage: 'assets/images/3main.jpg',
-    mainImageMin: 'assets/images/3mainMin.jpg',
-    secondImage: 'assets/images/3second.jpg',
-    secondImageMin: 'assets/images/3secondMin.jpg',
-    country: 'Netherlands Antilles',
-    city: 'Curacao',
-    telephone: 127777777,
-    temperature: 27,
-    waterTemperature: 16,
-    weather: 'windy',
-    followers: 12,
-    following: 100
-  }];
+
+  public puertoRico: Place = new Place('Hotel', 'Puerto Rico', 'San Juan', 123321123, 33, 21, 'sunny', 45, 3445, '1');
+
+  public frenchPolynesia: Place = new Place('SPA', 'French Polyneisa', 'Bora Bora', 123555123, 30, 25, 'windy', 12345, 405, '2');
+
+  public netherlands: Place = new Place('Another hotel', 'Netherlands Antilles', 'Curacao', 127777777, 27, 16, 'windy', 12, 100, '3');
 
   public place: Place;
 
   ngOnInit() {
-    this.place = this.places[0]
+    this.place = this.puertoRico;
   }
 
 }
--- src/app/place.ts
+++ src/app/place.ts
@@ -1,9 +1,5 @@
 export class Place {
   public name: string;
-  public mainImage: string;
-  public mainImageMin: string;
-  public secondImage: string;
-  public secondImageMin: string;
   public country: string;
   public city: string;
   public telephone: number;
@@ -12,4 +8,35 @@ export class Place {
   public weather: string;
   public followers: number;
   public following:number;
+  private imagePrefix: string;
+  private static imageUrlPath: string = 'assets/images/';
+
+  public constructor(name: string, country: string, city: string, telephone: number, temperature: number, waterTemperature: number, weather: string, followers: number, following: number, imagePrefix: string) {
+    this.name = name;
+    this.country = country;
+    this.city = city;
+    this.telephone = telephone;
+    this.temperature = temperature;
+    this.waterTemperature = waterTemperature;
+    this.weather = weather;
+    this.followers = followers;
+    this.following = following;
+    this.imagePrefix = imagePrefix;
+  }
+
+  public getMainImage():string {
+    return Place.imageUrlPath + this.imagePrefix + 'main.jpg'
+  }
+
+  public getMainMinImage():string {
+    return Place.imageUrlPath + this.imagePrefix + 'mainMin.jpg'
+  }
+
+  public getSecondImage():string {
+    return Place.imageUrlPath + this.imagePrefix + 'second.jpg'
+  }
+
+  public getSecondMinImage():string {
+    return Place.imageUrlPath + this.imagePrefix + 'secondMin.jpg'
+  }
 }
